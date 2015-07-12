62380013d3998dae4c71e891560752aab638d3b2
matsu-chara <matsuy00@gmail.com>
2015-07-12 19:50:16 +0900

    _なしでもアクセス可能にする

--- src/main/scala/OptionValueAccessor.scala
+++ src/main/scala/OptionValueAccessor.scala
@@ -6,19 +6,20 @@ object OptionValueAccessor {
 
   implicit def unwrapOptValue[A](a: OptValue[A]): Option[A] = a.unwrap
 
-  implicit class OptValue[A](val unwrap: Option[A]) extends Dynamic  {
+  implicit class OptValue[A](val unwrap: Option[A]) extends Dynamic {
     def wrap: OptValue[A] = new OptValue[A](unwrap)
+
     def selectDynamic(name: String): Any = macro Impl.Opt.selectDynamicImpl
   }
 
   object Impl {
+
     object Opt {
       def selectDynamicImpl(c: whitebox.Context)(name: c.Expr[String]): c.Tree = {
         import c.universe._
 
         val nameStr = name.tree match {
-          case pq"${n: String}" if n.startsWith("_") => n.drop(1)
-          case _ => c.abort(c.enclosingPosition, s"#$name not found.")
+          case pq"${n: String}" => n
         }
 
         c.typecheck(q"${c.prefix}.unwrap").tpe.typeArgs match {
@@ -29,5 +30,7 @@ object OptionValueAccessor {
         }
       }
     }
+
   }
+
 }
--- src/main/scala/OptionValueAccessorTest.scala
+++ src/main/scala/OptionValueAccessorTest.scala
@@ -3,15 +3,19 @@ import OptionValueAccessor._
 object OptionValueAccessorTest {
 
   case class A(foo: Int)
+
   case class B(bar: Option[A])
+
   case class C(hoge: Option[B])
+
   case class D(fuga: Option[C])
+
   case class E(value: D)
 
-  def main (args: Array[String]) {
+  def main(args: Array[String]) {
     val edcba: Option[E] = Some(E(D(Some(C(Some(B(Some(A(1)))))))))
 
-    val x: Option[Int] = OptValue(edcba).wrap._value._fuga._hoge._bar._foo
+    val x: Option[Int] = OptValue(edcba).wrap.value.fuga.hoge.bar.foo
     println(x)
   }
 }
